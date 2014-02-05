b2446b32e36a94bc8cc3645f63f95be108556e6b
Jan Markmann <jan.markmann@preis24.de>
2014-02-05 11:21:33 +0100

    Added mounting aliases

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,5 +1,8 @@
 alias git='LANGUAGE=en_US git'
 
+alias mount.gdrive='google-drive-ocamlfuse ~/mnt/gdrive'
+alias mount.soprado-dev='sshfs soprado-dev: ~/mnt/soprado-dev'
+
 alias rsync.from.soprado-dev='rsync -avrz --exclude="^var/cache/" --progress -e ssh --exclude=cache soprado-dev:work/p24-web/ /home/markmann/workspace/p24-web/'
 alias rsync.to.soprado-dev='rsync -avrz --exclude="var/cache/" --progress -e ssh --exclude=cache /home/markmann/workspace/p24-web/ soprado-dev:work/p24-web/'
 
