bffcc1213c0dc1514806765b874841075dfbf2cc
Jan Markmann <jan.markmann@preis24.de>
2014-05-06 10:00:49 +0200

    new rsync and fix for gwip

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,11 +1,5 @@
 alias git='LANGUAGE=en_US git'
 
-alias mount.gdrive='google-drive-ocamlfuse ~/mnt/gdrive'
-alias mount.soprado-dev='sshfs soprado-dev: ~/mnt/soprado-dev'
-
-alias rsync.from.soprado-dev='rsync -avrz --exclude="^var/cache/" --progress -e ssh --exclude=cache soprado-dev:work/p24-web/ /home/markmann/workspace/p24-web/'
-alias rsync.to.soprado-dev='rsync -avrz --exclude="var/cache/" --progress -e ssh --exclude=cache /home/markmann/workspace/p24-web/ soprado-dev:work/p24-web/'
-
 alias ack='ack-grep'
 alias sudp='sudo'
 
@@ -14,3 +8,6 @@ alias o='xdg-open'
 
 alias cya='sudo reboot'
 alias kthxbai='sudo halt'
+
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
--- custom/preis24.aliases.zsh
+++ custom/preis24.aliases.zsh
@@ -1,3 +1,8 @@
+alias mount.soprado-dev='sshfs soprado-dev: ~/mnt/soprado-dev'
+alias rsync.from.soprado-dev='rsync -avrz --exclude="^var/cache/" --progress -e ssh --exclude=cache soprado-dev:work/p24-web/ /home/markmann/workspace/p24-web/'
+alias rsync.to.soprado-dev='rsync -avrz --exclude="var/cache/" --progress -e ssh --exclude=cache /home/markmann/workspace/p24-web/ soprado-dev:work/p24-web/'
+alias rsync.from.soprado-dev2='rsync -avrz --exclude="^var/cache/" --progress -e ssh --exclude=cache soprado-dev:work/p24-web2/ /home/markmann/workspace/p24-web/'
+alias rsync.to.soprado-dev2='rsync -avrz --exclude="var/cache/" --progress -e ssh --exclude=cache /home/markmann/workspace/p24-web/ soprado-dev:work/p24-web2/'
 alias p24php.markmann='APPLICATION_ENV=markmann php'
 alias p24php.prodjoker='APPLICATION_ENV=prodjoker php'
 alias p24php='p24php.markmann'
