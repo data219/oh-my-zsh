f66ae3a36d270f37d50df176694a6e2f7518a0c3
Jan Markmann <jan.markmann@preis24.de>
2014-07-17 08:44:23 +0200

    new named folders

--- custom/named_folders.zsh
+++ custom/named_folders.zsh
@@ -2,8 +2,11 @@ downloads=~/Downloads
 dropbox=~/Dropbox
 workspace=~/workspace
 p24web=~/workspace/p24-web
-vagrant=~/vagrant
+testing=~/workspace/Testing
+vagrant=~/vagrant-boxes
+p24dev=~/vagrant-boxes/p24-dev
 images=~/Images
 desktop=~/Desktop
-gdrive=~/Google\ Drive
-: ~downloads ~dropbox ~workspace ~p24web ~vagrant ~images ~desktop ~gdrive
+gdrive=~/GoogleDrive
+sf=~/workspace/symfony
+: ~downloads ~dropbox ~workspace ~p24web ~testing ~vagrant ~images ~desktop ~gdrive ~sf ~p24dev
