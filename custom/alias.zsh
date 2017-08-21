7a9914f90de88f1426b7c930b8bf40665d1a7197
Jan Markmann <jan.markmann@cocomore.com>
2017-08-21 14:01:13 +0200

    new_stuff

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -34,8 +34,8 @@ alias qa-serve='bin/analyze serve --hostname=0.0.0.0 -p8888 NUL &!'
 alias delete.old.kernels="sudo apt-get autoremove --purge 'linux-image-[0-9].*' linux-image-$(uname -r)+"
 alias rm.bak="find . -type f -name '*~' -delete"
 
-alias yubilock.enable='sudo mv /etc/udev/rules.d/85-yubikey-screen-lock.rules.disabled /etc/udev/rules.d/85-yubikey-screen-lock.rules'
-alias yubilock.disable='sudo mv /etc/udev/rules.d/85-yubikey-screen-lock.rules /etc/udev/rules.d/85-yubikey-screen-lock.rules.disabled'
+alias yubilock.enable='sudo mv /etc/udev/rules.d/85-yubikey-screen-lock.rules.disabled /etc/udev/rules.d/85-yubikey-screen-lock.rules > /dev/null 2>&1'
+alias yubilock.disable='sudo mv /etc/udev/rules.d/85-yubikey-screen-lock.rules /etc/udev/rules.d/85-yubikey-screen-lock.rules.disabled > /dev/null 2>&1'
 alias yubilock.exemption='yubilock.disable && read -k1 -s "?Remove Yubikey and press any key to continue... " && yubilock.enable'
 alias ylen=yubilock.enable
 alias yldi=yubilock.disable
