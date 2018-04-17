845d3d4e0eb323b513ab4ec2f9ddf3244e4ee0a6
Jan Markmann <jan.markmann@meinauto.de>
2018-04-17 11:54:30 +0200

    work stuff

--- custom/named_folders_project.zsh
+++ custom/named_folders_project.zsh
@@ -1,5 +1,8 @@
 meinAutoApp=~/workspace/MeinAutoApp
 clientBundle=~/workspace/MeinAutoApp/vendor/meinautosymfony/clientbundle
 legacyBundle=~/workspace/MeinAutoApp/vendor/meinautosymfony/legacybundle
+erpApp=~/workspace/ERPApp
+eva30=~/workspace/eva-3.0
+eva31=~/workspace/eva-3.1
 
-: ~meinAutoApp ~clientBundle ~legacyBundle
+: ~meinAutoApp ~clientBundle ~legacyBundle ~erpApp ~eva30 ~eva31
--- custom/symfony2.zsh
+++ custom/symfony2.zsh
@@ -2,7 +2,7 @@ alias sfsh='sf -s'
 alias sf-server-start='sf server:start 0.0.0.0:8000'
 alias sf-server-stop='sf server:stop 0.0.0.0:8000'
 alias sf-server=sf-server-start
-alias sfunit='phpunit'
+alias sfunit='bin/phpunit'
 alias sf-php-cs-fixer='php-cs-fixer fix --verbose --level=symfony --fixers=align_equals,align_double_arrow,ordered_use,short_array_syntax --config=sf23 .'
 alias sf-php-cs-fixer-list='php-cs-fixer list --verbose --level=symfony --fixers=align_equals,align_double_arrow,ordered_use,short_array_syntax --config=sf23 .'
 alias sf-realclean='rm -rf var/cache/* && rm -rf vendor/* && composer install && sf cache:warm'
