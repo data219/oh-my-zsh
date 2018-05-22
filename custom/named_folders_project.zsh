e520fe832bc67c6f21f803c17735c377305bb3ee
Jan Markmann <jan.markmann@meinauto.de>
2018-05-22 13:43:52 +0200

    new named folders

--- custom/named_folders_project.zsh
+++ custom/named_folders_project.zsh
@@ -4,5 +4,7 @@ legacyBundle=~/workspace/MeinAutoApp/vendor/meinautosymfony/legacybundle
 erpApp=~/workspace/ERPApp
 eva30=~/workspace/eva-3.0
 eva31=~/workspace/eva-3.1
+proneuwagen=~/workspace/proneuwagen
+dealerScore=~/workspace/DealerScore
 
-: ~meinAutoApp ~clientBundle ~legacyBundle ~erpApp ~eva30 ~eva31
+: ~meinAutoApp ~clientBundle ~legacyBundle ~erpApp ~eva30 ~eva31 ~proneuwagen ~dealerScore
